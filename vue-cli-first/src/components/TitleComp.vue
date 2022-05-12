9228b372fb7f9e5fbaf7309b08b5a92e1b034533
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-05-12 13:46:10 +0200

    Esercizio completato!

---
+++ vue-cli-first/src/components/TitleComp.vue
@@ -0,0 +1,17 @@
+
+<template>
+    <h1>
+        La mia prima app con Vue CLI! Ciaone a tutti!
+    </h1>
+</template>
+
+<script>
+export default {
+  name: 'TitleComp',
+}
+</script>
+
+<style scoped>
+
+</style>
+
